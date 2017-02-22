7aba4a161c1d1134328364263a9ddff95559d119
Omar <oways@sec.aljazeera.tv>
2017-02-22 17:04:19 +0300

    Fixing bugs

--- drupal-checker.py
+++ drupal-checker.py
@@ -47,16 +47,17 @@ i = 0
 for y in urls:
 
 	try:
-		# connection timeout 1 sec
+		# connection timeout 3 sec
 		with eventlet.Timeout(3):
 			content = requests.get('http://%s/' % y.replace("\n",""))
 	except:
-		nothing = ""
+		pass
 	# check if the website contain drupal
 	if content:
-		if "http://drupal.org" in content.text:
+		if "drupal.org" in content.text:
 			drupals.append(y.replace("\n",""))
 		bar.update(i)
+	content = ""
 	i = i+1
 bar.finish()
 print "\n\nResult:\n-------------------\n"
@@ -65,7 +66,7 @@ for x in drupals:
 	a = os.popen('curl --connect-timeout 1 --max-time 1 http://%s/CHANGELOG.txt 2>/dev/null | grep -m2 .' % x).readlines()
 	try:
 		with eventlet.Timeout(3):
-			b = requests.get('http://%s/admin/views/ajax/autocomplete/user/w' % x)
+			b = requests.get('http://%s/admin/views/ajax/autocomplete/user/w' % x,allow_redirects=False)
 	except:
 		pass
 	
@@ -78,25 +79,9 @@ for x in drupals:
 			print colored('{0} [Drupal] ==> {1}'.format(x, a[1].replace("\n","").replace("Drupal","")), 'green')
 			version = re.search('[0-9.]+', a[1]).group()
 			print "Exploits: https://www.cvedetails.com/google-search-results.php?q=drupal+%s&sa=Search" % version
-		'''		if "8.0" in a[0]:
-			print "Exploits: https://www.cvedetails.com/vulnerability-list/vendor_id-1367/product_id-2387/version_id-192943/Drupal-Drupal-8.0.0.html"
-		else if "7.36" in a[0]:
-			print "Exploits: https://www.cvedetails.com/vulnerability-list/vendor_id-1367/product_id-2387/version_id-184069/Drupal-Drupal-7.36.html"
-		else if "7.35" in a[0]:
-			print "Exploits: https://www.cvedetails.com/vulnerability-list/vendor_id-1367/product_id-2387/version_id-184068/Drupal-Drupal-7.35.html"
-		else if "7.34" in a[0]:
-			print "Exploits: https://www.cvedetails.com/vulnerability-list/vendor_id-1367/product_id-2387/version_id-181083/Drupal-Drupal-7.34.html"
-		else if "7.33" in a[0]:
-			print "Exploits: https://www.cvedetails.com/vulnerability-list/vendor_id-1367/product_id-2387/version_id-175956/Drupal-Drupal-7.33.html"
-		else if "7.32" in a[0]:
-			print "Exploits: https://www.cvedetails.com/vulnerability-list/vendor_id-1367/product_id-2387/version_id-192976/Drupal-Drupal-7.32.html"
-		else if "7.31" in a[0]:
-			print "Exploits: https://www.cvedetails.com/vulnerability-list/vendor_id-1367/product_id-2387/version_id-192977/Drupal-Drupal-7.31.html"
-		else if "7.30" in a[0]:
-			print "Exploits: https://www.cvedetails.com/vulnerability-list/vendor_id-1367/product_id-2387/version_id-169916/Drupal-Drupal-7.30.html"'''
 	else:
 		print colored('{0} [Drupal] ==> Version Not Found'.format(x), 'yellow')
-	if 403 != b.status_code:
+	if 403 != b.status_code and b.status_code != 302:
 		print "vuln to autocomplete exploit ==> http://%s/admin/views/ajax/autocomplete/user/w" % x
 
 
